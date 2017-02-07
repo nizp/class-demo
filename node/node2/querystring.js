14d3660437c07d129650c6804e2c9d90406f5065
nizp <nzp890524@163.com>
2017-02-07 23:30:58 +0800

    change

--- node/node2/querystring.js
+++ node/node2/querystring.js
@@ -1,8 +1,13 @@
 const http = require('http');
+//专门处理url地址为不带?后面的数据[user=nizp&pass=123]
 // const queryStr = require('querystring');
+//专门处理接收数据中带?的数据[xxx.com?user=nizp&pass=123]
 const queryStr = require('url');
 http.createServer(function(req,res){
   //var q = queryStr.parse(req.url.split('?')[1]);
-  var q = queryStr.parse(req.url,true);
+  var q = queryStr.parse(req.url,true); //true代表将字符串转为对象
+  //q返回值有2个常用属性
+  //1.q.pathname  地址
+  //2.q.query   转化的对象
   console.log(q.query);
 }).listen(8080);
